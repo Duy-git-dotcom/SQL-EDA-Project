fb62be523ae2e681eebc21d276b41ef4aba64165
Dao Khanh Duy <daokhanhduy01012001@gmail.com>
2025-05-08 22:11:34 +0700

    Add files via upload

---
+++ Project_EDA/Query/Advanced Data_Analytics.sql
@@ -0,0 +1,299 @@
+﻿USE DataWarehouseAnalytics
+GO
+
+SELECT * FROM [gold.dim_customers]
+SELECT * FROM [gold.dim_products]
+SELECT * FROM [gold.fact_sales]
+GO
+
+
+----CHANGE OVER TIME
+---SALES OVER ORDER DATE
+--YEAR
+SELECT 
+	YEAR(order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS customer_count,
+	SUM(quantity) AS total_quantity
+FROM [gold.fact_sales]
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY YEAR(order_date) ASC
+GO
+--MONTH,YEAR
+SELECT 
+	DATETRUNC(MONTH,order_date) AS order_year,
+	SUM(sales_amount) AS total_sales,
+	COUNT(DISTINCT customer_key) AS customer_count,
+	SUM(quantity) AS total_quantity
+FROM [gold.fact_sales]
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(MONTH,order_date) 
+ORDER BY DATETRUNC(MONTH,order_date)  ASC
+GO
+
+--SUBQUERY
+SELECT MAX(total_sales) FROM (
+	SELECT 
+		YEAR(order_date) AS order_year,
+		MONTH(order_date) AS order_month,
+		SUM(sales_amount) AS total_sales,
+		COUNT(DISTINCT customer_key) AS customer_count,
+		SUM(quantity) AS total_quantity
+	FROM [gold.fact_sales]
+	WHERE order_date IS NOT NULL
+	GROUP BY YEAR(order_date), MONTH(order_date)
+) AS CTE
+GO
+
+
+----CUMULATIVE ANALYSIS
+---RUNNING TOTAL SALES(ROLLING TOTAL) TỔNG
+SELECT 
+	date_time,
+	total_sales,
+	SUM(total_sales) OVER(ORDER BY date_time) AS Rolling_total
+FROM (
+	SELECT
+		DATETRUNC(MONTH, order_date) AS date_time,
+		SUM(sales_amount) AS total_sales
+	FROM [gold.fact_sales]
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+) AS SUB
+ORDER BY date_time ASC
+GO
+---RUNNING TOTAL SALES(ROLLING TOTAL) THEO NĂM
+SELECT 
+	date_time,
+	total_sales,
+	SUM(total_sales) OVER(PARTITION BY YEAR(date_time) ORDER BY date_time) AS Rolling_total
+FROM (
+	SELECT
+		DATETRUNC(MONTH, order_date) AS date_time,
+		SUM(sales_amount) AS total_sales
+	FROM [gold.fact_sales]
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+) AS SUB
+ORDER BY date_time ASC
+GO
+---MOVING AVERAGE
+SELECT 
+	date_time,
+	total_sales,
+	SUM(total_sales) OVER(ORDER BY date_time) AS Rolling_total,
+	AVG_Price,
+	AVG(AVG_Price) OVER(ORDER BY date_time) AS Moving_average
+FROM (
+	SELECT
+		DATETRUNC(MONTH, order_date) AS date_time,
+		SUM(sales_amount) AS total_sales,
+		AVG(price) AS AVG_Price
+	FROM [gold.fact_sales]
+	WHERE order_date IS NOT NULL
+	GROUP BY DATETRUNC(MONTH, order_date)
+) AS SUB
+ORDER BY date_time ASC
+GO
+
+----PERFORMANCE ANALYSIS
+---(Phân tích so sánh kết quả kinh doanh của năm nay với năm trước, hiện tại với doanh số trung bình của sản phẩm)
+--year_to_year
+WITH yearly_sales AS(
+	SELECT 
+		YEAR(sa.order_date) AS order_year,
+		pr.product_name,
+		SUM(sa.sales_amount) AS current_sales
+	FROM [gold.fact_sales] AS sa
+	LEFT JOIN [gold.dim_products] AS pr
+	ON sa.product_key = pr.product_key
+	WHERE sa.order_date IS NOT NULL
+	GROUP BY
+		YEAR(sa.order_date),
+		pr.product_name
+)
+SELECT
+	order_year,
+	product_name,
+	current_sales,
+	AVG(current_sales) OVER(PARTITION BY product_name) AS avg_sales,
+	current_sales - AVG(current_sales) OVER(PARTITION BY product_name) AS diff_avg,
+	CASE WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) > 0 THEN 'Above Average'
+		WHEN current_sales - AVG(current_sales) OVER(PARTITION BY product_name) < 0 THEN 'Below Average'
+		ELSE 'Average'
+	END AS Category_avg,
+	LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS Previous_year_sales,
+	current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) AS year_diff,
+	CASE WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) > 0 THEN 'Increase'
+		WHEN current_sales - LAG(current_sales) OVER(PARTITION BY product_name ORDER BY order_year) < 0 THEN 'Decrease'
+		ELSE 'No_change'
+	END AS Category_year
+FROM yearly_sales
+ORDER BY product_name, order_year ASC
+GO
+
+----PART_TO_WHOLE
+---Thể loại sản phẩm nào chiếm nhiều phần trăm nhất trong tổng lượng bán
+WITH category_sales AS (
+	SELECT
+	category,
+	SUM(sales_amount) AS total_sales
+	FROM [gold.fact_sales]  AS sa
+	LEFT JOIN [gold.dim_products] AS pr
+	ON sa.product_key = pr.product_key
+	GROUP BY category
+)
+SELECT 
+category,
+total_sales,
+SUM(total_sales) OVER() AS over_sales,
+CONCAT(ROUND(((CAST (total_sales AS FLOAT))/SUM(total_sales) OVER())*100,2), '%') AS Percentage_sales
+FROM category_sales
+ORDER BY total_sales DESC
+GO
+
+----Data Segmentation
+---Phân loại sản phẩm
+WITH products_segmentation AS (
+	SELECT
+	product_key,
+	product_name,
+	cost,
+	CASE WHEN cost < 100 THEN 'Below 100'
+		WHEN cost BETWEEN 100 AND 500 THEN '100-500'
+		WHEN cost BETWEEN 500 AND 1000 THEN '500-1000'
+		ELSE 'Above 1000'
+	END AS cost_range
+	FROM [gold.dim_products]
+)
+SELECT 
+cost_range,
+COUNT(product_key) AS total_products
+FROM products_segmentation
+GROUP BY cost_range
+ORDER BY total_products DESC
+GO
+
+---Phân chia tệp khách hàng thành VIP, Regular, New dựa trên thời gian tạo tài khoản và số tiền đã tiêu xài
+WITH date_table AS(
+	SELECT
+	cu.customer_key,
+	SUM(sales_amount) AS total_sales,
+	MIN(order_date) AS first_date,
+	MAX(order_date) AS last_date,
+	DATEDIFF(month,MIN(order_date),MAX(order_date)) AS order_time
+	FROM [gold.fact_sales] AS sa
+	LEFT JOIN  [gold.dim_customers] AS cu
+	ON sa.customer_key = cu.customer_key
+	GROUP BY cu.customer_key
+)
+SELECT
+segmentation_cus,
+COUNT(customer_key) AS total_cus
+FROM (
+	SELECT
+	customer_key,
+	CASE WHEN total_sales > 5000 AND order_time >= 12 THEN 'VIP'
+		 WHEN total_sales <= 5000 AND order_time >= 12 THEN 'Regular'
+		 ELSE 'New'
+	END AS segmentation_cus
+	FROM date_table
+) AS t
+GROUP BY segmentation_cus
+ORDER BY total_cus DESC
+GO
+
+----BÁO CÁO 
+/*
+============================================================
+
+Mục đích:
+- Báo cáo này tổng hợp các chỉ số và hành vi quan trọng của khách hàng.
+
+Các chỉ tiêu:
+1. Thu thập các trường dữ liệu thiết yếu như tên, tuổi và chi tiết giao dịch.
+2. Phân loại khách hàng thành các nhóm (VIP, Thường xuyên, Mới) và theo nhóm tuổi.
+3. Tổng hợp các chỉ số ở cấp độ khách hàng:
+   - Tổng số đơn hàng
+   - Tổng doanh thu
+   - Tổng số lượng đã mua
+   - Tổng số sản phẩm
+   - Vòng đời khách hàng (tính theo tháng)
+4. KPI:
+   - Gần đây nhất (số tháng kể từ lần đặt hàng cuối cùng)
+   - Giá trị đơn hàng trung bình
+   - Chi tiêu trung bình hàng tháng
+
+----------------------------------------------------------
+
+CREATE VIEW gold.report_view AS
+WITH basic_query AS
+(
+	SELECT
+		cu.customer_id,
+		cu.customer_key,
+		cu.customer_number,
+		CONCAT(cu.first_Name,' ',cu.last_name) AS fullname,
+		DATEDIFF(year, cu.birthdate, GETDATE()) AS age,
+		sa.order_number,
+		sa.product_key,
+		sa.order_date,
+		sa.sales_amount,
+		sa.quantity
+	FROM [gold.fact_sales] AS sa
+	LEFT JOIN [gold.dim_customers] AS cu
+	ON sa.customer_key = cu.customer_key
+	WHERE sa.order_date IS NOT NULL
+	),
+customer_aggregation AS (
+	SELECT 
+		customer_key,
+		customer_number,
+		fullname,
+		age,
+		COUNT(DISTINCT order_number) AS total_orders,
+		SUM(sales_amount) AS total_sales,
+		SUM(quantity) AS Total_quantity,
+		COUNT(DISTINCT product_key) AS total_products,
+		MIN(order_date) AS first_date,
+		MAX(order_date) AS last_date,
+		DATEDIFF(month,MIN(order_date),MAX(order_date)) AS order_time
+	FROM basic_query
+	GROUP BY 
+		customer_key,
+		customer_number,
+		fullname,
+		age
+)
+SELECT
+	customer_key,
+	customer_number,
+	fullname,
+	age,
+	CASE WHEN age < 20 THEN 'Under 20'
+		WHEN age BETWEEN 20 AND 29 THEN '20-29'
+		WHEN age BETWEEN 30 AND 39 THEN '30-39'
+		WHEN age BETWEEN 40 AND 49 THEN '40-49'
+		ELSE 'Above 50'
+	END AS age_segmentation,
+	total_orders,
+	total_sales,
+	Total_quantity,
+	total_products,
+	CASE WHEN total_sales > 5000 AND order_time >= 12 THEN 'VIP'
+		 WHEN total_sales <= 5000 AND order_time >= 12 THEN 'Regular'
+		 ELSE 'New'
+	END AS segmentation_cus,
+	---Số tháng từ lần cuối đặt sản phẩm
+	last_date,
+	DATEDIFF(month,last_date,GETDATE()) AS Recency,
+	---Số tiền chi trung bình cho 1 đơn hàng
+	CASE WHEN total_sales = 0 THEN 0
+		ELSE total_sales/total_orders 
+	END AS Avg_sales,
+	---Số tiền chi trung bình trên tháng từ lúc bắt đầu mua các sản phẩm
+	CASE WHEN order_time = 0 THEN total_sales
+		ELSE total_sales/order_time 
+	END AS Avg_monthly
+FROM customer_aggregation
---
+++ Project_EDA/Query/Basic_EDA.sql
@@ -0,0 +1,191 @@
+﻿USE DataWarehouseAnalytics
+GO
+
+SELECT * FROM [gold.dim_customers]
+SELECT * FROM [gold.dim_products]
+SELECT * FROM [gold.fact_sales]
+GO
+
+----KIỂM TRA DATABASE
+--ĐỐI TƯỢNG TRONG DATABASE
+SELECT * FROM INFORMATION_SCHEMA.TABLES
+GO
+
+--CỘT TRONG DATABASE
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+GO
+
+----KIỂM TRA CHIỀU DỮ LIỆU KHÔNG LIÊN TỤC(DIMENSION EXPLORATION)
+--XUẤT THÂN KHÁCH HÀNG
+SELECT DISTINCT country FROM [gold.dim_customers]
+SELECT country, COUNT(*) FROM [gold.dim_customers]
+GROUP BY country
+GO
+
+--CÁC LOẠI HÌNH SẢN PHẨM CỦA DOANH NGHIỆP
+SELECT DISTINCT category, subcategory, product_name FROM [gold.dim_products]
+ORDER BY 1,2,3
+GO
+
+----KIỂM TRA DỮ LIỆU ĐO LƯỜNG (MEASURE EXPLORATION)
+-- TÌM TỔNG DOANH SỐ
+SELECT SUM(sales_amount) AS Total_Sales FROM dbo.[gold.fact_sales]
+-- TÌM SỐ LƯỢNG SẢN PHẨM ĐÃ BÁN
+SELECT SUM(quantity) AS Total_Quantity FROM dbo.[gold.fact_sales]
+-- TÌM GIÁ BÁN TRUNG BÌNH
+SELECT AVG(price) AS AVG_Price FROM dbo.[gold.fact_sales]
+-- TÌM TỔNG SỐ ĐƠN HÀNG
+SELECT COUNT(DISTINCT order_number) AS Total_Order FROM dbo.[gold.fact_sales]
+-- TÌM TỔNG SỐ SẢN PHẨM
+SELECT COUNT(DISTINCT product_key) AS Total_Products FROM dbo.[gold.fact_sales]
+-- TÌM TỔNG SỐ KHÁCH HÀNG
+SELECT COUNT(customer_key) AS Total_Customers FROM dbo.[gold.dim_customers]
+-- TÌM TỔNG SỐ KHÁCH HÀNG ĐÃ TỪNG ĐẶT HÀNG
+SELECT COUNT(DISTINCT customer_key) AS Ordered_Customers FROM dbo.[gold.fact_sales]
+GO
+
+----KHÁM PHÁ DỮ LIỆU THỜI GIAN 
+--SỐ THỜI GIAN KỂ TỪ LẦN BÁN SẢN PHẨM ĐẦU TIÊN TỚI LẦN CUỐI
+SELECT 
+MIN(order_date) AS min_orderdate,
+MAX(order_date) AS max_orderdate,
+DATEDIFF(month,MIN(order_date),MAX(order_date)) AS datediff_order
+FROM dbo.[gold.fact_sales]
+GO
+
+--ĐỘ TUỔI CỦA KHÁCH HÀNG
+SELECT 
+MIN(birthdate) AS Oldest,
+DATEDIFF(year,MIN(birthdate),GETDATE()) AS Oldest_Age,
+MAX(birthdate) AS Youngest,
+DATEDIFF(year,MAX(birthdate),GETDATE()) AS Youngest_Age
+FROM dbo.[gold.dim_customers]
+GO
+
+--TRUY VẤN BÁO CÁO TẤT CẢ NHỮNG THÔNG SỐ CẦN THIẾT
+SELECT 'Total Sales' AS Measure_name, SUM(sales_amount) AS Measure_value FROM dbo.[gold.fact_sales]
+UNION ALL
+SELECT 'Total Quantity' , SUM(quantity) FROM dbo.[gold.fact_sales]
+UNION ALL
+SELECT 'Average Price', AVG(price) FROM dbo.[gold.fact_sales]
+UNION ALL
+SELECT 'Total Nr. Orders', COUNT(DISTINCT order_number) FROM dbo.[gold.fact_sales]
+UNION ALL
+SELECT 'Total Nr. Products', COUNT(product_name) FROM dbo.[gold.dim_products]
+UNION ALL
+SELECT 'Total Nr. Customers', COUNT(customer_key) FROM dbo.[gold.dim_customers]
+GO
+
+
+----SO SÁNH CÁC BIẾN MEASURES THEO BIẾN DIMENSION
+-- TÌM TỔNG SỐ KHÁCH HÀNG THEO QUỐC GIA 
+SELECT 
+country,
+COUNT(customer_key) AS Total_Customers
+FROM dbo.[gold.dim_customers]
+GROUP BY country
+ORDER BY Total_Customers DESC
+GO
+
+-- TÌM TỔNG SỐ KHÁCH HÀNG THEO GIỚI TÍNH
+SELECT 
+gender,
+COUNT(customer_key) AS Total_Customers
+FROM dbo.[gold.dim_customers]
+GROUP BY gender
+ORDER BY Total_Customers DESC
+GO
+
+-- TÌM TỔNG SỐ SẢN PHẨM THEO LOẠI MỤC 
+SELECT 
+category,
+COUNT(product_key) AS Total_Products
+FROM dbo.[gold.dim_products]
+GROUP BY category
+ORDER BY Total_Products DESC
+GO
+-- CHI PHÍ TRUNG BÌNH CHO TỪNG LOẠI SẢN PHẨM LÀ BAO NHIÊU? 
+SELECT 
+category,
+AVG(cost) AS AVG_cost
+FROM dbo.[gold.dim_products]
+GROUP BY category
+ORDER BY AVG_cost DESC
+GO
+
+-- TỔNG DOANH THU CHO TỪNG LOẠI SẢN PHẨM LÀ BAO NHIÊU? 
+SELECT 
+pr.category,
+SUM(sa.price-pr.cost) AS Total_Revenue
+FROM dbo.[gold.dim_products] AS pr
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON pr.product_key = sa.product_key
+GROUP BY pr.category
+ORDER BY Total_Revenue DESC
+GO
+
+-- TÌM TỔNG DOANH THU ĐẠT ĐƯỢC BỞI TỪNG KHÁCH HÀNG
+SELECT 
+cu.customer_key,
+cu.first_name,
+cu.last_name,
+SUM(sa.sales_amount) AS Total_Revenue
+FROM dbo.[gold.dim_customers] AS cu
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON cu.customer_key = sa.customer_key
+GROUP BY 
+cu.customer_key,
+cu.first_name,
+cu.last_name
+ORDER BY Total_Revenue DESC
+GO
+
+-- PHÂN PHỐI CỦA CÁC MẶT HÀNG ĐÃ BÁN CHO CÁC QUỐC GIA LÀ GÌ?
+SELECT 
+cu.country,
+SUM(sa.quantity) AS Total_Sold_Items
+FROM dbo.[gold.dim_customers] AS cu
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON cu.customer_key = sa.customer_key
+GROUP BY cu.country
+ORDER BY Total_Sold_Items DESC
+GO
+
+----PHÂN TÍCH PHÂN HẠNG
+--5 SẢN PHẨM CÓ DOANH THU CAO NHẤT
+SELECT 
+TOP 5 pr.product_name,
+SUM(sa.sales_amount) AS Total_Revenue,
+ROW_NUMBER() OVER(ORDER BY SUM(sa.sales_amount) ASC) AS Rankings
+FROM dbo.[gold.dim_products] AS pr
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON pr.product_key = sa.product_key
+GROUP BY pr.product_name
+GO 
+
+--5 SẢN PHẨM CÓ DOANH THU THẤP NHẤT
+SELECT 
+TOP 5 pr.product_name,
+SUM(sa.sales_amount) AS Total_Revenue
+FROM dbo.[gold.dim_products] AS pr
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON pr.product_key = sa.product_key
+GROUP BY pr.product_name
+ORDER BY Total_Revenue ASC
+GO 
+
+--TOP 3 NGƯỜI CÓ ÍT ĐƠN ĐẶT HÀNG NHẤT 
+SELECT TOP 3
+cu.customer_key,
+cu.first_name,
+cu.last_name,
+COUNT(DISTINCT order_number) AS total_orders
+FROM dbo.[gold.dim_customers] AS cu
+RIGHT JOIN dbo.[gold.fact_sales] AS sa
+ON cu.customer_key = sa.customer_key
+GROUP BY 
+cu.customer_key,
+cu.first_name,
+cu.last_name
+ORDER BY total_orders 
+GO
